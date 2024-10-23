e42de47f94ec5b8e10c4bd61acc6973f41654db0
xtreemfire-stok <arhipovio@mail.ru>
2024-10-23 23:59:08 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,73 @@
+"""Задача №1"""
+
+# n = int(input())
+# m = int(input())
+
+# print((m + n - 1)//n)
+
+"""Задача №3"""
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# s1 = (a + 1)//2
+# s2 = (b + 1)//2
+# s3 = (c + 1)//2
+
+# print(s1 + s2 + s3)
+
+"""Задача №5"""
+
+# i = int(input())
+# j = int(input())
+
+# if i - j == 0:
+#     print(-1)
+# else:
+#     print(i + j - 1)
+
+"""Задача №7"""
+
+# n = int(input())
+
+# if n % 4 == 0 and n % 100 != 0 or n % 400 == 0:
+#     print("yes")
+# else:
+#     print("no")
+
+"""Домашнее задание. Задача №1"""
+
+# a = int(input())
+
+# a1 = a % 10
+# a = a // 10
+# a2 = a % 10
+# a = a // 10
+
+# print(a + a1 + a2)
+
+"""Задача №2"""
+
+# s = int(input())
+
+
+
+
+
+
+"""Задача №3"""
+
+a = input()
+
+a1 = int(a[0])
+b1 = int(a[1])
+c1 = int(a[2])
+a2 = int(a[3])
+b2 = int(a[4])
+c2 = int(a[5])
+
+if  a1 + b1 + c1 == a2 + b2 + c2:
+    print("yes")
+else:
+    print("no")
